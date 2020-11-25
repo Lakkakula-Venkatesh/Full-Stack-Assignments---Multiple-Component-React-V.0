8594a6e6a76f810c349fdd0b1884cc934fb5a474
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-11-25 12:45:00 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     return (
       <>
-      <h2 data-ns-test="project-name">{'Project Name'}</h2>
-      <p data-ns-test="project-description">{'Project Description'}</p>
+      <h2 data-ns-test="project-name">Project Name</h2>
+      <p data-ns-test="project-description">Project Description</p>
       </>
     );
   }
